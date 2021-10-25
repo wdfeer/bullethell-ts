fd58c88099a26d00e208a6073ae1dad1af26c69a
wdf33r <49950781+wdfeer@users.noreply.github.com>
2021-10-25 21:02:44 +0300

    Added new colliders

--- src/colliders.ts
+++ src/colliders.ts
@@ -1,12 +1,40 @@
-class Hitbox {
-    width: number;
-    height: number;
-    position: Vector2 = Vector2.Zero;
-    constructor(width: number, height: number, position?: Vector2) {
-        this.width = width;
-        this.height = height;
-        if (position)
-            this.position = position;
+abstract class Hitbox {
+    position: Vector2;
+    public abstract get center(): Vector2;
+    constructor(position: Vector2) {
+        this.position = position;
+    }
+    abstract colliding(point: Vector2): boolean;
+}
+class Circle extends Hitbox {
+    radius: number;
+    public get center(): Vector2 {
+        return new Vector2(this.position.x + this.radius, this.position.y + this.radius);
+    }
+    constructor(position: Vector2, radius: number) {
+        super(position);
+        this.radius = radius;
+    }
+    colliding(point: Vector2) {
+        return point.Sub(this.center).length <= this.radius;
     }
 }
+class Rectangle extends Hitbox {
+    width: number;
+    height: number;
+    public get center(): Vector2 {
+        return new Vector2(this.position.x + this.width, this.position.y + this.height);
+    }
+    constructor(position: Vector2, width: number, height: number) {
+        super(position);
+        this.width = width;
+        this.height = height;
+    }
+    colliding(point: Vector2) {
+        let relativePos = point.Sub(this.position);
+        return relativePos.x <= this.width && relativePos.x >= 0 && relativePos.y <= this.height && relativePos.y >= 0;
+    }
+}
+
+let hitboxGetters: (() => Hitbox)[] = [];
 
--- wwwroot/colliders.js
+++ wwwroot/colliders.js
@@ -1,11 +1,63 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Hitbox = /** @class */ (function () {
-    function Hitbox(width, height, position) {
-        this.position = Vector2.Zero;
-        this.width = width;
-        this.height = height;
-        if (position)
-            this.position = position;
+    function Hitbox(position) {
+        this.position = position;
     }
     return Hitbox;
 }());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(position, radius) {
+        var _this = _super.call(this, position) || this;
+        _this.radius = radius;
+        return _this;
+    }
+    Object.defineProperty(Circle.prototype, "center", {
+        get: function () {
+            return new Vector2(this.position.x + this.radius, this.position.y + this.radius);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Circle.prototype.colliding = function (point) {
+        return point.Sub(this.center).length <= this.radius;
+    };
+    return Circle;
+}(Hitbox));
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(position, width, height) {
+        var _this = _super.call(this, position) || this;
+        _this.width = width;
+        _this.height = height;
+        return _this;
+    }
+    Object.defineProperty(Rectangle.prototype, "center", {
+        get: function () {
+            return new Vector2(this.position.x + this.width, this.position.y + this.height);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Rectangle.prototype.colliding = function (point) {
+        var relativePos = point.Sub(this.position);
+        return relativePos.x <= this.width && relativePos.x >= 0 && relativePos.y <= this.height && relativePos.y >= 0;
+    };
+    return Rectangle;
+}(Hitbox));
+var hitboxGetters = [];
