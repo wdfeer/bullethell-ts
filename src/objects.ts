ac39f222d47414b1182170401a88c569849dd1df
wdf33r <49950781+wdfeer@users.noreply.github.com>
2021-10-27 21:13:32 +0300

    Added bullet hell

--- src/objects.ts
+++ src/objects.ts
@@ -59,6 +59,34 @@ abstract class enemy extends body {
 }
 class boss1 extends enemy {
     speed = 6;
+    onPlayerHit = () => {
+        pl.hp -= 100;
+    }
+    get attackCooldown(): number {
+        return 40 + 80 / Math.sqrt(pl.score + 1);
+    }
+    attackTimer = 0;
+    ai = () => {
+        let diff: Vector2 = pl.center.Sub(this.center);
+        let dist: number = diff.length;
+        if (dist > 200 + 1000 * Math.random())
+            this.velocity = pl.center.Sub(this.center).normalized.Mult(this.speed);
+        this.attackTimer++;
+        if (this.attackTimer >= this.attackCooldown){
+            this.rangedAttack();
+            this.attackTimer = 0;
+        }
+    }
+    rangedAttack() {
+        let bullets = shootEvenlyInACircle(3 + Math.floor(Math.sqrt(pl.score)), 19, this.center, 5 + 10 * Math.random());
+        bullets.forEach(b => {
+            bodies.push(b);
+            drawings.push((ctx) => {
+                drawCircle(ctx, b.radius, b.center, 'black');
+                fillCircle(ctx, b.radius, b.center, 'red');
+            })
+        });
+    }
     constructor(center: Vector2, radius: number) {
         super(center, radius);
         drawings.push((ctx) => {
@@ -66,28 +94,34 @@ class boss1 extends enemy {
             fillCircle(ctx, this.radius * 0.9, this.center, 'black');
         });
     }
+}
+class bullet extends enemy {
     onPlayerHit = () => {
         pl.hp -= 100;
     }
-    ai = () => {
-        let diff: Vector2 = pl.center.Sub(this.center);
-        let dist: number = diff.length;
-        if (dist > 200 + 1000 * Math.random())
-            this.velocity = pl.center.Sub(this.center).normalized.Mult(this.speed);
-    }
-}
-class bullet extends enemy {
     constructor(center: Vector2, velocity: Vector2, radius: number) {
         super(center, radius);
         this.velocity = velocity;
     }
+}
+function shootEvenlyInACircle(count: number, bulletRadius: number, pos: Vector2, velocity: number, spawnRadius: number = 0): bullet[] {
+    let bullets: bullet[] = [];
+    let angle = 0;
+    for (let i = 0; i < count; i++) {
+        let Vy = Math.sin(angle) * velocity;
+        let Vx = Math.cos(angle) * velocity;
+        let V = new Vector2(Vx, Vy);
+        bullets.push(new bullet(pos.Add(V.normalized.Mult(spawnRadius)), V, bulletRadius))
+        angle += 360 / count;
+    }
+    return bullets;
 }
 class coin {
     static radius = 25;
     draw: draw;
     drawingId: number;
     collider: CircleCollider;
-    deleteDrawing(){
+    deleteDrawing() {
         delete drawings[this.drawingId];
     }
     onPlayerCollide: () => void = () => {
@@ -107,7 +141,7 @@ class coin {
     };
     constructor(public pos: Vector2) {
         this.draw = ctx => {
-            drawCircle(ctx, coin.radius, pos, 'brown');
+            drawCircle(ctx, coin.radius, pos, 'green');
             fillCircle(ctx, coin.radius, pos, '#ffffde');
         };
         this.drawingId = drawings.length;
